2553654c6bc51f14b31ae91d54b9facd1c40b075
Documenter.jl <documenter@juliadocs.github.io>
2023-06-22 16:24:22 +0000

    build based on a5e4979

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Polyagon.jl-Documentation","page":"Polyagon.jl Documentation","title":"Polyagon.jl Documentation","text":"","category":"section"},{"location":"","page":"Polyagon.jl Documentation","title":"Polyagon.jl Documentation","text":"greet()","category":"page"},{"location":"#Polyagon.greet-Tuple{}","page":"Polyagon.jl Documentation","title":"Polyagon.greet","text":"greet()\n\nReturns \"Hello World\".\n\n\n\n\n\n","category":"method"}]
+}
